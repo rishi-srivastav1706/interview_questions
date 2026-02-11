7b50bc321bb418db847c91fce5653388f690fc2b
Rishi Srivastav <rishiaiweb@gmail.com>
2026-02-11 13:58:57 +0530

    Add files via upload
    You have a blue lottery ticket, with int a, b and c on it. This makes there pairs, which  will call ab, bc, ac. Consider thee sum of the number in the each pair. If any pair sum to exactly 10, the result is 10. Otherwise if the a or b or c is more than 10, the result is 5. Otherwise the result is 0

---
+++ blue_lottery.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("enter the number: ");
+    scanf("%d", &a);
+    printf("enter the number: ");
+    scanf("%d", &b);
+    printf("enter the number: ");
+    scanf("%d", &c);
+    int ab = a+b;
+    int bc = b+c;
+    int ac = a+c;
+    if(ab==10 || bc==10 || ac==10){
+        printf("10");
+    }
+    else if(a>10 || b>0 || c>10){
+        printf("5");
+    }
+    else{
+        printf("0");
+    }
+
+    return 0;
+}
